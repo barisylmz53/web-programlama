10c6dff6b3f629863e49e3104e8c3f8b19ba22a2
Baris Yilmaz <devsaskral@gmail.com>
2022-12-20 21:29:38 +0300

    Template Alındı Düzenlemeler Yapıldı

---
+++ WEBODEVI/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBODEVI.Models;
+
+namespace WEBODEVI.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
