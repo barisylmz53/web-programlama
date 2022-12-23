9ef76c05d86c4d4070274fc3bb4c3a4af04db2e3
Baris Yilmaz <devsaskral@gmail.com>
2022-12-23 19:12:53 +0300

    Database bağlantısı yapıldı

---
+++ WEBODEVI/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace WEBODEVI.Models
+{
+    public class Context:DbContext
+    {
+        public DbSet<Uye> Uyeler { get; set; }
+        public DbSet<Araba> Arabalar { get; set; }
+
+    }
+}
---
+++ WEBODEVI/Models/Uye.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEBODEVI.Models
+{
+    public class Uye
+    {
+        [Key]
+        public int UyeID { get; set; }
+        public string? UyeAd { get; set; }
+        public string? UyeSoyad { get; set; }
+        public string? UyeMail { get; set; }
+        public string? UyeSifre { get; set; }
+    }
+}
