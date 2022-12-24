48d17ae9dd2da567176bb9cd9abb35492ea53f70
Baris Yilmaz <devsaskral@gmail.com>
2022-12-24 11:53:45 +0300

    Migrations oluşturuldu.

---
+++ WEBODEVI/Migrations/20221224085053_uye_araba.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEBODEVI.Migrations
+{
+    /// <inheritdoc />
+    public partial class uyearaba : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arabalar",
+                columns: table => new
+                {
+                    CarID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarBrand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarModel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarPrice = table.Column<int>(type: "int", nullable: false),
+                    CarPhoto = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arabalar", x => x.CarID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Uyeler",
+                columns: table => new
+                {
+                    UyeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UyeAd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UyeSoyad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UyeMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UyeSifre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uyeler", x => x.UyeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Arabalar");
+
+            migrationBuilder.DropTable(
+                name: "Uyeler");
+        }
+    }
+}
--- WEBODEVI/Models/Context.cs
+++ WEBODEVI/Models/Context.cs
@@ -5,6 +5,11 @@ namespace WEBODEVI.Models
 {
     public class Context:DbContext
     {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-9991\\SQLEXPRESS;Database=CarDB;integrated security = true;");
+        }
         public DbSet<Uye> Uyeler { get; set; }
         public DbSet<Araba> Arabalar { get; set; }
 
